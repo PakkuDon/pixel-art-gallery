b7ae789ac102f7bdc13f7a002903686b6a72e8c9
PakkuDon <pakku.pd@gmail.com>
2020-10-11 00:13:21 +1100

    Add piece from October 10 2020

---
+++ data.js
@@ -0,0 +1,10 @@
+export default [
+  {
+    src: 'img/20201010-lospecdaily.png',
+    description: `
+      Lospec Dailies prompt: #magic
+      Palette: [MonoMetalic](https://lospec.com/palette-list/monometalic)
+    `,
+    date: '2020-10-10',
+  }
+]
