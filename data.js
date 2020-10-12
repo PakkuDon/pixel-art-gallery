9b57d1066d7d55f4a541041259ca301c37eb4c68
PakkuDon <pakku.pd@gmail.com>
2020-10-12 23:32:35 +1100

    Add piece from October 12 2020

--- data.js
+++ data.js
@@ -6,5 +6,13 @@ export default [
       Palette: [MonoMetalic](https://lospec.com/palette-list/monometalic)
     `,
     date: '2020-10-10',
+  },
+  {
+    src: 'img/20201012-lospecdaily.png',
+    description: `
+      Lospec Dailies prompt: #landscape
+      Palette: [Nintendo Gameboy (Black Zero)](https://lospec.com/palette-list/nintendo-gameboy-black-zero)
+    `,
+    date: '2020-10-12',
   }
 ]
