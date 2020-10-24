17638b0a540daa26b4c5952d4fc1133b94cb2a95
PakkuDon <pakku.pd@gmail.com>
2020-10-25 00:00:07 +1100

    Rename app.js to default Webpack entry point

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: 'production',
-  entry: './src/app.js',
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
