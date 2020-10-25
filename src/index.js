01a98ed617c602354e9ca9851ea252cdda6faf87
PakkuDon <pakku.pd@gmail.com>
2020-10-26 00:07:28 +1100

    Add placeholder root for React app

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
-console.log('Hello world')
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>Hello world</div>
+)
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#app'),
+)
