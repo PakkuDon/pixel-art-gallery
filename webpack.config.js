606e05256c6d8bffca7d55073c3ea8c7509a4f7a
PakkuDon <pakku.pd@gmail.com>
2020-10-25 23:57:34 +1100

    Run JS files through Babel on build

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 
 module.exports = {
   mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
