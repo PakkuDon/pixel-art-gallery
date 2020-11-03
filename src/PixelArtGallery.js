11d6d76b9ed3872e76b69169901124d378f6fec9
PakkuDon <pakku.pd@gmail.com>
2020-11-03 12:53:10 +1100

    Set selected image to ID in route if present

--- src/PixelArtGallery.js
+++ src/PixelArtGallery.js
@@ -1,12 +1,22 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
 
 import Sidebar from "./components/Sidebar"
 import ImageDetails from "./components/ImageDetails"
-
+import extractFilename from "./util/extractFilename"
 import pixelArtEntries from "./data"
 
 const PixelArtGallery = () => {
   const [selectedImage, setSelectedImage] = useState(pixelArtEntries[0])
+  const { id } = useParams()
+
+  useEffect(() => {
+    if (id && selectedImage?.src !== id) {
+      setSelectedImage(
+        pixelArtEntries.find((image) => id === extractFilename(image.src))
+      )
+    }
+  }, [])
 
   return (
     <React.Fragment>
