40e35119304ca7f2072ddc2f0fa81599f8d09ce4
PakkuDon <pakku.pd@gmail.com>
2020-11-28 23:10:58 +1100

    Display tags under image

--- src/components/ImageDetails/ImageDetails.js
+++ src/components/ImageDetails/ImageDetails.js
@@ -17,6 +17,7 @@ const ImageDetails = ({ image }) => (
         __html: marked(image.description.replace(/ +/g, " ")),
       }}
     />
+    <div>Tags: {image.tags.join(", ")}</div>
     <p>- Posted {image.date}</p>
   </Card>
 )
