9160938449c60a8969d2ff25b802986a71909cb1
PakkuDon <pakku.pd@gmail.com>
2020-12-29 19:22:29 +1100

    Add script to generate RSS feed

---
+++ src/util/generateRssFeed.js
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import fs from "fs"
+import path from "path"
+import marked from "marked"
+
+import pixelArtEntries from "../data"
+import extractFilename from "./extractFilename"
+
+marked.setOptions({
+  breaks: true,
+  gfm: true,
+})
+
+const outputFile = path.join(__dirname, "../../dist/feed.xml")
+
+const feedItems = pixelArtEntries.map((pixelArt) => {
+  const filename = extractFilename(pixelArt.src)
+  const title = pixelArt.description.split("\n")[1].trim()
+
+  return `
+    <item>
+      <title>${title}</title>
+      <description>
+        <img src="https://pakkudon.github.io/pixel-art-gallery/img/${
+          pixelArt.src
+        }" alt="${filename}" />
+        ${marked(pixelArt.description.replace(/ +/g, " "))}
+      </description>
+      <link>https://pakkudon.github.io/pixel-art-gallery/${filename}</link>
+      <pubDate>${new Date(pixelArt.date).toGMTString()}</pubDate>
+    </item>
+  `
+})
+
+const xml = `
+  <?xml version="1.0" encoding="ISO-8859-1" ?>
+  <rss version="2.0">
+    <channel>
+      <title>Pixel Art Gallery</title>
+      <link>https://pakkudon.github.io/pixel-art-gallery/</link>
+      <description>Collection of pixel art pieces</description>
+      <pubDate>${new Date().toGMTString()}</pubDate>
+      ${feedItems.join("\n")}
+    </channel>
+  </rss>
+`
+
+fs.writeFile(outputFile, xml, (error) => {
+  if (error) {
+    console.error(error)
+  } else {
+    console.log(`RSS feed written to ${outputFile}`)
+  }
+})
