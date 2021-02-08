38079449c40fbb0c1c52e5bde191d5475ac44fce
PakkuDon <pakku.pd@gmail.com>
2021-02-08 23:38:37 +1100

    Wrap RSS feed generation in a function to make it easier to test

--- src/util/generateRssFeed.js
+++ src/util/generateRssFeed.js
@@ -6,46 +6,49 @@ import marked from "marked"
 import pixelArtEntries from "../data"
 import extractFilename from "./extractFilename"
 
-marked.setOptions({
-  breaks: true,
-  gfm: true,
-  xhtml: true,
-})
+const generateRssFeed = (entries = []) => {
+  marked.setOptions({
+    breaks: true,
+    gfm: true,
+    xhtml: true,
+  })
+
+  const feedItems = entries.map((pixelArt) => {
+    const filename = extractFilename(pixelArt.src)
+    const title = pixelArt.description.split("\n")[1].trim()
+
+    return `
+      <item>
+        <title>${title}</title>
+        <description>
+          <![CDATA[<img src="https://pakkudon.github.io/pixel-art-gallery/img/${
+            pixelArt.src
+          }" alt="${filename}" />]]>
+          ${marked(pixelArt.description.replace(/ +/g, " "))}
+        </description>
+        <link>https://pakkudon.github.io/pixel-art-gallery/${filename}</link>
+        <pubDate>${new Date(pixelArt.date).toGMTString()}</pubDate>
+      </item>
+    `
+  })
+
+  return `<?xml version="1.0" encoding="ISO-8859-1" ?>
+    <rss version="2.0">
+      <channel>
+        <title>Pixel Art Gallery</title>
+        <link>https://pakkudon.github.io/pixel-art-gallery/</link>
+        <description>Collection of pixel art pieces</description>
+        <pubDate>${new Date().toGMTString()}</pubDate>
+        ${feedItems.join("\n")}
+      </channel>
+    </rss>
+  `
+}
 
 const outputFile = path.join(__dirname, "../../dist/feed.xml")
 
-const feedItems = pixelArtEntries.map((pixelArt) => {
-  const filename = extractFilename(pixelArt.src)
-  const title = pixelArt.description.split("\n")[1].trim()
-
-  return `
-    <item>
-      <title>${title}</title>
-      <description>
-        <![CDATA[<img src="https://pakkudon.github.io/pixel-art-gallery/img/${
-          pixelArt.src
-        }" alt="${filename}" />]]>
-        ${marked(pixelArt.description.replace(/ +/g, " "))}
-      </description>
-      <link>https://pakkudon.github.io/pixel-art-gallery/${filename}</link>
-      <pubDate>${new Date(pixelArt.date).toGMTString()}</pubDate>
-    </item>
-  `
-})
-
-const xml = `<?xml version="1.0" encoding="ISO-8859-1" ?>
-  <rss version="2.0">
-    <channel>
-      <title>Pixel Art Gallery</title>
-      <link>https://pakkudon.github.io/pixel-art-gallery/</link>
-      <description>Collection of pixel art pieces</description>
-      <pubDate>${new Date().toGMTString()}</pubDate>
-      ${feedItems.join("\n")}
-    </channel>
-  </rss>
-`
-
 if (process.env.NODE_ENV !== "test") {
+  const xml = generateRssFeed(pixelArtEntries)
   fs.writeFile(outputFile, xml, (error) => {
     if (error) {
       console.error(error)
@@ -54,3 +57,5 @@ if (process.env.NODE_ENV !== "test") {
     }
   })
 }
+
+export default generateRssFeed
