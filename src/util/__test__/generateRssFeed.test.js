632c3443ff157a8d7b90458ed49df31273ae1906
PakkuDon <pakku.pd@gmail.com>
2021-02-09 00:04:44 +1100

    Force current time in tests to ensure snapshot remains consistent
    RSS feed includes current time in output. This is
    liable to change between test runs

--- src/util/__test__/generateRssFeed.test.js
+++ src/util/__test__/generateRssFeed.test.js
@@ -1,6 +1,11 @@
 import generateRssFeed from "../generateRssFeed"
 
 describe("generateRssFeed", () => {
+  beforeAll(() => {
+    jest.useFakeTimers("modern")
+    jest.setSystemTime(new Date("2020-01-01").getTime())
+  })
+
   it("generates RSS feed containing pixel art entries", () => {
     const entries = [
       {
