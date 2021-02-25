4e6ae067ed6bf9306004f367c5f5164fcb243ffe
PakkuDon <pakku.pd@gmail.com>
2021-02-25 22:35:45 +1100

    Clone index.html rather than redirecting to it when generating pages
    Previously we were redirecting to index.html on load.
    This leads to a somewhat sluggish experience as users need
    to load one page, execute some JS and then land on
    another page with more JS
    
    ADR to follow

--- script/build.sh
+++ script/build.sh
@@ -8,8 +8,8 @@ yarn run webpack --config config/production.config.js
 echo "Copying images to dist/..."
 cp -r img/ dist/img/
 
-echo "Generating dist files with redirects"
-yarn run babel-node src/util/generateDistRedirects.js
+echo "Generating pages for each entry"
+yarn run babel-node src/util/generatePages.js
 
 echo "Generating RSS feed"
 yarn run babel-node src/util/generateRssFeed.js
--- src/util/generatePages.js
+++ src/util/generatePages.js
@@ -5,6 +5,9 @@ import path from "path"
 import pixelArtEntries from "../data"
 import extractFilename from "./extractFilename"
 
+const baseHtmlPath = path.join(__dirname, "../../dist/index.html")
+const baseHtml = fs.readFileSync(baseHtmlPath, "utf8")
+
 pixelArtEntries.forEach((pixelArt) => {
   const filename = extractFilename(pixelArt.src)
   const outputFilePath = path.join(__dirname, `../../dist/${filename}.html`)
@@ -12,27 +15,13 @@ pixelArtEntries.forEach((pixelArt) => {
     .split("\n")[1]
     .trim()
     .replace(/"/g, "&quot;")
-  const html = `
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Pixel Art Gallery - ${filename}</title>
+
+  const previewMetatags = `
     <meta property="og:image" content="https://pakkudon.github.io/pixel-art-gallery/img/${pixelArt.src}" />
     <meta property="og:description" content="${metaDescription}" />
-  </head>
-  <body>
-    <p>
-      Redirecting you to Pixel Art Gallery. Click <a href="https://pakkudon.github.io/pixel-art-gallery">here</a> if you are not redirected automatically.
-    </p>
-    <script>
-      let redirectTarget = window.location.href.replace("${filename}", '')
-      window.location.replace(redirectTarget)
-    </script>
-  </body>
-</html>
   `
+
+  const html = baseHtml.replace("</head>", `${previewMetatags}</head>`)
   fs.writeFile(outputFilePath, html, (error) => {
     if (error) {
       console.error(error)
