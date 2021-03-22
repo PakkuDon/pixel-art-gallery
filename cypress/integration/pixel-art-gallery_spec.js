b3ddbcfbab75e62b16d1bcb8aea7d74170e108f9
PakkuDon <pakku.pd@gmail.com>
2021-03-22 23:21:39 +1100

    Add basic smoke test to confirm page loads and images can be selected

---
+++ cypress/integration/pixel-art-gallery_spec.js
@@ -0,0 +1,32 @@
+const pixelArtEntries = require("../../src/data")
+
+const selectRandomEntry = () => {
+  const index = Math.floor(Math.random() * pixelArtEntries.length)
+  return pixelArtEntries[index]
+}
+
+describe("Pixel Art Gallery", () => {
+  let testEntry = selectRandomEntry()
+  let testEntryIdentifier = testEntry.src.split(".")[0]
+
+  beforeEach(() => {
+    cy.visit("/")
+  })
+
+  it("renders successfully", () => {
+    cy.get("#app").should("exist")
+  })
+
+  it("displays pixel art entries", () => {
+    cy.get(`a[href='/${testEntryIdentifier}'`).should("exist")
+  })
+
+  context("when pixel art entry is selected", () => {
+    it("displays details about pixel art entry", () => {
+      cy.get(`a[href='/${testEntryIdentifier}'`).click()
+      cy.get(".pixelArt")
+        .should("have.attr", "src")
+        .should("contain", testEntry.src)
+    })
+  })
+})
