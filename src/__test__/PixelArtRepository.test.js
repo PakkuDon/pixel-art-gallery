78ce817b8f7803befcacd8ea731a2892a7347c57
PakkuDon <pakku.pd@gmail.com>
2021-05-02 23:13:34 +1000

    Add tests for PixelArtRepository

---
+++ src/__test__/PixelArtRepository.test.js
@@ -0,0 +1,41 @@
+import PixelArtRepository from "../PixelArtRepository"
+
+describe("PixelArtRepository", () => {
+  describe(".findAll", () => {
+    describe("when there are entries that match predicate", () => {
+      const entry = {
+        src: "img.png",
+        title: "foo",
+      }
+      const otherEntry = {
+        src: "img.jpg",
+        title: "bar",
+      }
+
+      beforeAll(() => {
+        PixelArtRepository.load([entry, otherEntry])
+      })
+
+      it("returns matching entries", () => {
+        expect(
+          PixelArtRepository.findAll((image) => image.title === "foo")
+        ).toEqual([entry])
+      })
+    })
+
+    describe("when there are no entries that match predicate", () => {
+      beforeAll(() => {
+        PixelArtRepository.load([
+          {
+            src: "img.png",
+            title: "foo",
+          },
+        ])
+      })
+
+      it("returns empty array", () => {
+        expect(PixelArtRepository.findAll(() => false)).toEqual([])
+      })
+    })
+  })
+})
