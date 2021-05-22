0cbeee5815932da397204aa267729586d7d60301
PakkuDon <pakku.pd@gmail.com>
2021-05-23 01:27:53 +1000

    Add redirects for pixel art aliases
    This will enable us to rename pixel art files while
    still retaining any previous URLs. ADR to follow

--- src/util/generatePages.js
+++ src/util/generatePages.js
@@ -24,15 +24,39 @@ pixelArtEntries.forEach((pixelArt) => {
   `
   const title = `Pixel Art Gallery - ${filename}`
 
-  const html = baseHtml
+  const pageHtml = baseHtml
     .replace("</head>", `${previewMetatags}</head>`)
     .replace("<title>Pixel Art Gallery</title>", `<title>${title}</title>`)
 
-  fs.writeFile(outputFilePath, html, (error) => {
+  fs.writeFile(outputFilePath, pageHtml, (error) => {
     if (error) {
       console.error(error)
     } else {
       console.log(`Wrote to dist/${filename}.html`)
     }
+  })
+
+  const aliases = pixelArt.aliases || []
+
+  aliases.forEach((alias) => {
+    const aliasFilePath = path.join(__dirname, `../../dist/${alias}.html`)
+    const redirectSnippet = `
+    <p>
+      Redirecting you to Pixel Art Gallery. Click <a href="https://pakkudon.github.io/pixel-art-gallery">here</a> if you are not redirected automatically.
+    </p>
+    <script>
+      let redirectTarget = window.location.href.replace('${alias}', '${filename}')
+      console.log(redirectTarget)
+      window.location.replace(redirectTarget)
+    </script>
+    `
+    const htmlForRedirect = pageHtml.replace("</body>", redirectSnippet)
+    fs.writeFile(aliasFilePath, htmlForRedirect, (error) => {
+      if (error) {
+        console.error(error)
+      } else {
+        console.log(`Wrote to dist/${filename}.html`)
+      }
+    })
   })
 })
