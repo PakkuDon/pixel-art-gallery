846e1ae75efcd821c0ee538bfc33abf35b0f2ca3
PakkuDon <pakku.pd@gmail.com>
2021-08-08 21:39:31 +1000

    Convert PixelArtRepository to TypeScript

---
+++ src/PixelArtRepository.ts
@@ -0,0 +1,19 @@
+import pixelArtEntries, { PixelArtEntry } from "./data"
+
+interface Repository {
+  entries: PixelArtEntry[]
+  load: (entries: PixelArtEntry[]) => void
+  findAll: (predicate?: () => boolean) => PixelArtEntry[]
+}
+
+const PixelArtRepository: Repository = {
+  entries: [],
+  load(entries: PixelArtEntry[] = pixelArtEntries) {
+    this.entries = entries
+  },
+  findAll(predicate = () => true) {
+    return this.entries.filter(predicate)
+  },
+}
+
+export default PixelArtRepository
