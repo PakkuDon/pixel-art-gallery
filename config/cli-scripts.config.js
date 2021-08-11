7bf17e54d80ed982f697c0e08a992efc23dfa630
PakkuDon <pakku.pd@gmail.com>
2021-08-12 00:06:42 +1000

    Add Webpack config to transpile Node scripts
    Previously these were transpiled using babel-node.
    As some files have been migrated to TypeScript and we
    are using TypeScript Loader to handle those
    we now need to use Webpack to handle these imports

---
+++ config/cli-scripts.config.js
@@ -0,0 +1,44 @@
+const CaseSensitivePathsPlugin = require("case-sensitive-paths-webpack-plugin")
+const path = require("path")
+
+module.exports = {
+  mode: "production",
+  entry: [
+    path.resolve(__dirname, "../src/util/generatePages.js"),
+    path.resolve(__dirname, "../src/util/generateRssFeed.js"),
+  ],
+  output: {
+    path: path.resolve(__dirname, "../script/generated"),
+    filename: "generateArtifacts.js",
+  },
+  target: "node",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+          {
+            loader: "ts-loader",
+          },
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"],
+  },
+  plugins: [
+    new CaseSensitivePathsPlugin(),
+  ],
+}
