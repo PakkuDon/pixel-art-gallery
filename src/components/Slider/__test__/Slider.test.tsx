f675a171044a67916627bc4781938cc1dcd40f74
PakkuDon <pakku.pd@gmail.com>
2021-09-12 23:42:50 +1000

    Add tests for <Slider />

---
+++ src/components/Slider/__test__/Slider.test.tsx
@@ -0,0 +1,39 @@
+import React from "react"
+import { screen, render, fireEvent } from "@testing-library/react"
+
+import Slider from "../Slider"
+
+describe("Slider", () => {
+  it("renders slider with provided props", () => {
+    const props = {
+      zoom: 50,
+      min: 0,
+      max: 100,
+      onChange: () => {},
+    }
+
+    render(<Slider {...props} />)
+    const slider = screen.getByRole("slider")
+
+    expect(parseInt(slider.getAttribute("value") as string, 10)).toBe(
+      props.zoom
+    )
+    expect(parseInt(slider.getAttribute("min") as string, 10)).toBe(props.min)
+    expect(parseInt(slider.getAttribute("max") as string, 10)).toBe(props.max)
+  })
+
+  it("calls onChange handler when slider is moved", () => {
+    const props = {
+      zoom: 50,
+      min: 0,
+      max: 100,
+      onChange: jest.fn(),
+    }
+
+    render(<Slider {...props} />)
+    const slider = screen.getByRole("slider")
+    fireEvent.change(slider, { target: { value: 10 } })
+
+    expect(props.onChange).toHaveBeenCalledWith(10)
+  })
+})
