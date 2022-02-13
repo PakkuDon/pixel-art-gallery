9f4c7fb26d73aee5151dc9b883999419fb3410d4
PakkuDon <pakku.pd@gmail.com>
2022-02-14 01:12:45 +1100

    Add # symbol before tags

--- src/components/ImageDetails/ImageDetails.tsx
+++ src/components/ImageDetails/ImageDetails.tsx
@@ -76,7 +76,7 @@ const ImageDetails = ({
             <strong>Tags: </strong>
             {image.tags.map((tag, index) => (
               <>
-                <Link to={`?q=${tag}`} key={`tag-${tag}`}>
+                <Link to={`?q=${tag}`} key={`tag-${tag}`} className="tag">
                   {tag}
                 </Link>
                 {index < image.tags.length - 1 && ", "}
