3b1b8f3fa60704bea9a47ea9009734a5066d6cf0
PakkuDon <pakku.pd@gmail.com>
2022-04-13 00:26:09 +1000

    Add tests for <SidebarEntry />

---
+++ src/components/Sidebar/SidebarEntry/SidebarEntry.test.tsx
@@ -0,0 +1,84 @@
+import React from "react"
+import { MemoryRouter } from "react-router-dom"
+import { screen, render } from "@testing-library/react"
+
+import SidebarEntry from "./SidebarEntry"
+
+describe("SidebarEntry", () => {
+  it("renders thumbnail and link to image", () => {
+    const img = {
+      src: "abc123.png",
+      title: "Test image",
+      date: "2000-01-01 00:00 UTC+1000",
+      tags: ["test"],
+    }
+
+    render(
+      <MemoryRouter>
+        <SidebarEntry entry={img} />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByRole("link")).toHaveAttribute("href", "/abc123")
+    expect(screen.getByRole("img")).toHaveAttribute("src", `img/${img.src}`)
+  })
+
+  it("highlights image if selected", () => {
+    const img = {
+      src: "abc123.png",
+      title: "Test image",
+      date: "2000-01-01 00:00 UTC+1000",
+      tags: ["test"],
+    }
+
+    render(
+      <MemoryRouter>
+        <SidebarEntry entry={img} isSelected />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByRole("link")).toHaveClass("selected")
+  })
+
+  it("appends search query to link", () => {
+    const img = {
+      src: "abc123.png",
+      title: "Test image",
+      date: "2000-01-01 00:00 UTC+1000",
+      tags: ["test"],
+    }
+    const searchQuery = "pixel"
+
+    render(
+      <MemoryRouter>
+        <SidebarEntry entry={img} searchQuery={searchQuery} />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      `/abc123?q=${searchQuery}`
+    )
+  })
+
+  it("appends search query to link as is if it contains special characters", () => {
+    const img = {
+      src: "abc123.png",
+      title: "Test image",
+      date: "2000-01-01 00:00 UTC+1000",
+      tags: ["test"],
+    }
+    const searchQuery = "something else #tags"
+
+    render(
+      <MemoryRouter>
+        <SidebarEntry entry={img} searchQuery={searchQuery} />
+      </MemoryRouter>
+    )
+
+    expect(screen.getByRole("link")).toHaveAttribute(
+      "href",
+      `/abc123?q=${searchQuery}`
+    )
+  })
+})
