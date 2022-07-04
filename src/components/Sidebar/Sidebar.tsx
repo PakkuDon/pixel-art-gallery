392ee3cde34ca0837ac3ee6f6e2bbd9992a65f05
PakkuDon <pakku.pd@gmail.com>
2022-07-04 23:43:51 +1000

    Replace dropdown list on smaller screens with carousel of past entries

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -62,30 +62,6 @@ const Sidebar = ({
           ))}
         </div>
       </div>
-      <div className="entriesDropdown">
-        <select
-          onChange={(event) => {
-            const selectedOption = entries.find(
-              (entry) =>
-                extractFilename(entry.src) ===
-                extractFilename(event.target.value)
-            )
-            if (selectedOption) {
-              onImageSelect(selectedOption)
-            }
-          }}
-          value={extractFilename(selectedImage?.src)}
-        >
-          {entries.map((entry) => {
-            const filename = extractFilename(entry.src)
-            return (
-              <option key={`option-${filename}`} value={filename}>
-                {filename}
-              </option>
-            )
-          })}
-        </select>
-      </div>
       <div className="entriesList">
         {entries.map((entry) => {
           const filename = extractFilename(entry.src)
