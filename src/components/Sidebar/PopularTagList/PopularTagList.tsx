91b4bdfa72bd59f15e2bf5c44e683a1c60659c7c
PakkuDon <pakku.pd@gmail.com>
2022-08-13 22:57:16 +1000

    Add button to hide or collapse popular tag list

--- src/components/Sidebar/PopularTagList/PopularTagList.tsx
+++ src/components/Sidebar/PopularTagList/PopularTagList.tsx
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "react-router-dom"
 
 const TAGS_TO_DISPLAY = 10
@@ -7,18 +7,32 @@ interface PopularTagListProps {
   countByTag: Array<{ tag: string; count: number }>
 }
 
-const PopularTagList = ({ countByTag }: PopularTagListProps) => (
-  <div>
-    Frequently used tags:{" "}
-    {countByTag.slice(0, TAGS_TO_DISPLAY).map(({ tag, count }, index) => (
-      <React.Fragment key={`tag-with-count-${tag}`}>
-        <Link to={`?q=${tag}`}>
-          {tag} ({count})
-        </Link>
-        {index < countByTag.length - 1 && ", "}
-      </React.Fragment>
-    ))}
-  </div>
-)
+const PopularTagList = ({ countByTag }: PopularTagListProps) => {
+  const [visible, setVisible] = useState(false)
+
+  if (visible) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setVisible(false)}>Hide most used tags</button>
+        </div>
+        {countByTag.slice(0, TAGS_TO_DISPLAY).map(({ tag, count }, index) => (
+          <React.Fragment key={`tag-with-count-${tag}`}>
+            <Link to={`?q=${tag}`}>
+              {tag} ({count})
+            </Link>
+            {index < countByTag.length - 1 && ", "}
+          </React.Fragment>
+        ))}
+      </div>
+    )
+  }
+
+  return (
+    <div>
+      <button onClick={() => setVisible(true)}>Show most used tags</button>
+    </div>
+  )
+}
 
 export default PopularTagList
