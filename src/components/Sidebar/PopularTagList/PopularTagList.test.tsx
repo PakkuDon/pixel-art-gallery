2688d604460927b2e440c9df483d6989d45ce43a
PakkuDon <pakku.pd@gmail.com>
2022-08-14 15:46:24 +1000

    Add tests for <PopularTagList />

---
+++ src/components/Sidebar/PopularTagList/PopularTagList.test.tsx
@@ -0,0 +1,71 @@
+import React from "react"
+import { MemoryRouter } from "react-router-dom"
+import { screen, render, fireEvent } from "@testing-library/react"
+
+import PopularTagList from "./PopularTagList"
+
+describe("PopularTagList", () => {
+  it("renders an expandable list of tags", async () => {
+    const tagList = [
+      { tag: "someTag", count: 3 },
+      { tag: "anotherTag", count: 1 },
+    ]
+
+    render(
+      <MemoryRouter>
+        <PopularTagList countByTag={tagList} />
+      </MemoryRouter>
+    )
+
+    fireEvent.click(screen.getByText(/Show/))
+    expect(screen.getByText(/someTag .*3/)).toBeInTheDocument()
+    expect(screen.getByText(/anotherTag .*1/)).toBeInTheDocument()
+    expect(screen.getByText(/Hide/)).toBeInTheDocument()
+
+    fireEvent.click(screen.getByText(/Hide/))
+    expect(screen.getByText(/Show/)).toBeInTheDocument()
+    expect(screen.queryByText(/someTag .*3/)).not.toBeInTheDocument()
+  })
+
+  it("renders link to filter entries by tag and number of times tag is used", () => {
+    const tagList = [{ tag: "someTag", count: 3 }]
+
+    render(
+      <MemoryRouter>
+        <PopularTagList countByTag={tagList} />
+      </MemoryRouter>
+    )
+
+    fireEvent.click(screen.getByText(/Show/))
+    expect(screen.queryByRole("link", { name: /someTag .*3/ })).toHaveAttribute(
+      "href",
+      "/?q=someTag"
+    )
+  })
+
+  it("displays first 10 tags only", () => {
+    const tagList = [
+      { tag: "tag1", count: 1 },
+      { tag: "tag2", count: 1 },
+      { tag: "tag3", count: 1 },
+      { tag: "tag4", count: 1 },
+      { tag: "tag5", count: 1 },
+      { tag: "tag6", count: 1 },
+      { tag: "tag7", count: 1 },
+      { tag: "tag8", count: 1 },
+      { tag: "tag9", count: 1 },
+      { tag: "tag10", count: 1 },
+      { tag: "tag11", count: 1 },
+    ]
+
+    render(
+      <MemoryRouter>
+        <PopularTagList countByTag={tagList} />
+      </MemoryRouter>
+    )
+
+    fireEvent.click(screen.getByText(/Show/))
+    expect(screen.getByText(/tag10/)).toBeInTheDocument()
+    expect(screen.queryByText(/tag11/)).not.toBeInTheDocument()
+  })
+})
