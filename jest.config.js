13f94a92248ae63e23eb543c4d281a7db25d80f1
PakkuDon <pakku.pd@gmail.com>
2022-11-21 00:02:54 +1100

    Use UTC timezone in tests
    Tests are currently failing in CI due to
    the runner using UTC and my machine being on GMT+11

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,8 @@
 /** @type {import('@ts-jest/dist/types').InitialOptionsTsJest} */
 
+// Use UTC time in tests
+process.env.TZ = "UTC"
+
 module.exports = {
   clearMocks: true,
   coverageProvider: "v8",
