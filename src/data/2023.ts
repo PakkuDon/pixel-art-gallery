ee302e91ce565d5deaa12b9155718e074570c084
PakkuDon <pakku.pd@gmail.com>
2023-01-01 23:53:06 +1100

    Add 20230101-pixeldailies

---
+++ src/data/2023.ts
@@ -0,0 +1,15 @@
+import { PixelArtEntry } from "."
+
+const entries: PixelArtEntry[] = [
+  {
+    src: "2023/20230101-pixeldailies.png",
+    title: "Pixel Dailies prompt: #Hut",
+    description: `
+      Palette: [Nostalgia48](https://lospec.com/palette-list/nostalgia48)
+    `,
+    date: "2023-01-01T23:53+11:00",
+    tags: ["pixel_dailies", "Hut", "landscape", "beach", "nostalgia48"],
+  },
+]
+
+export default entries
--- src/data/index.ts
+++ src/data/index.ts
@@ -1,6 +1,7 @@
 import entries2020 from "./2020"
 import entries2021 from "./2021"
 import entries2022 from "./2022"
+import entries2023 from "./2023"
 
 export interface PixelArtEntry {
   src: string
@@ -15,6 +16,7 @@ const entries: PixelArtEntry[] = [
   ...entries2020,
   ...entries2021,
   ...entries2022,
+  ...entries2023,
 ]
 
 export default entries
