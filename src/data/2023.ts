9a41a86100e3269346c7144af5b195cdb2ba2e26
PakkuDon <pakku.pd@gmail.com>
2023-01-03 16:40:14 +1100

    Add 20230103-pixeldailies

--- src/data/2023.ts
+++ src/data/2023.ts
@@ -10,6 +10,15 @@ const entries: PixelArtEntry[] = [
     date: "2023-01-01T23:53+11:00",
     tags: ["pixel_dailies", "Hut", "landscape", "beach", "nostalgia48"],
   },
+  {
+    src: "2023/20230103-pixeldailies.png",
+    title: "Pixel Dailies prompt: #carving",
+    description: `
+      Palette: [Nostalgia48](https://lospec.com/palette-list/nostalgia48)
+    `,
+    date: "2023-01-03T16:40+11:00",
+    tags: ["pixel_dailies", "carving", "owl", "wood", "nostalgia48"],
+  },
 ]
 
 export default entries
