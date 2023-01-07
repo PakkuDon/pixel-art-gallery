15dbad82199edd6abea407d1078b4c5a2e624788
PakkuDon <pakku.pd@gmail.com>
2023-01-07 23:56:25 +1100

    List all tag usage counts on stats page

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,11 +1,19 @@
 import React from "react"
 import { Link } from "react-router-dom"
+
+import PixelArtRepository from "../PixelArtRepository"
 import Card from "../components/Card"
+import TagList from "../components/Sidebar/TagList"
 
 const Statistics = () => (
   <div className="single-column-layout">
     <Card>
-      <Link to="/">Back to gallery</Link>
+      <div>
+        <Link to="/">Back to gallery</Link>
+      </div>
+      <div>
+        <TagList countByTag={PixelArtRepository.countByTag()} />
+      </div>
     </Card>
   </div>
 )
