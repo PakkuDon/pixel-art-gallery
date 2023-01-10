e7a3f136bef5ee9dcb4c68310d506db5ba79d448
PakkuDon <pakku.pd@gmail.com>
2023-01-10 22:38:09 +1100

    Display number of entries posted per year and per month

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -5,17 +5,44 @@ import PixelArtRepository from "../PixelArtRepository"
 import Card from "../components/Card"
 import TagList from "../components/Sidebar/TagList"
 
-const Statistics = () => (
-  <div className="single-column-layout">
-    <Card>
-      <div>
-        <Link to="/">Back to gallery</Link>
-      </div>
-      <div>
-        <TagList countByTag={PixelArtRepository.countByTag()} />
-      </div>
-    </Card>
-  </div>
-)
+const Statistics = () => {
+  const countByTag = PixelArtRepository.countByTag()
+  const countByYear = PixelArtRepository.countBy((entry) =>
+    new Date(entry.date).getFullYear().toString()
+  ).sort((a, b) => a.key.localeCompare(b.key))
+  const countByMonth = PixelArtRepository.countBy((entry) => {
+    const date = new Date(entry.date)
+    return `${date.getFullYear()}-${date.getMonth() + 1}`
+  }).sort((a, b) => a.key.localeCompare(b.key))
+
+  return (
+    <div className="single-column-layout">
+      <Card>
+        <div>
+          <Link to="/">Back to gallery</Link>
+        </div>
+        <div>
+          <TagList countByTag={countByTag} />
+        </div>
+        <div>
+          <h2>Entries posted each year</h2>
+          {countByYear.map(({ key, count }) => (
+            <div key={key}>
+              <strong>{key}:</strong> {count}
+            </div>
+          ))}
+        </div>
+        <div>
+          <h2>Entries posted each month</h2>
+          {countByMonth.map(({ key, count }) => (
+            <div key={key}>
+              <strong>{key}:</strong> {count}
+            </div>
+          ))}
+        </div>
+      </Card>
+    </div>
+  )
+}
 
 export default Statistics
