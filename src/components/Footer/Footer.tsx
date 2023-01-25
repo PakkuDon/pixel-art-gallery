35a738e76d032529177c17b0a083377aa3d3918c
PakkuDon <pakku.pd@gmail.com>
2023-01-25 22:09:08 +1100

    Add link to stats page to footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,4 +1,5 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import classnames from "classnames"
 
 import "./Footer.css"
@@ -21,6 +22,9 @@ const Footer = ({ prefersDarkTheme, onDarkThemeToggle }: FooterProps) => (
           />{" "}
           Dark theme?
         </label>
+      </li>
+      <li>
+        <Link to="/stats">Portfolio stats</Link>
       </li>
       <li>
         <a href="./feed.xml" target="_blank" rel="noopener noreferrer">
