c916493925a4d20b926292e5e4ee423eb4db87b7
PakkuDon <pakku.pd@gmail.com>
2023-02-09 23:59:35 +1100

    Remove ellipsis from build script

--- script/build.sh
+++ script/build.sh
@@ -5,19 +5,19 @@ set -euo pipefail
 if [ -n "$(git status --porcelain | grep 'M src/data')" ]; then
   read -p "src/data contains uncomitted changes which will be published. Are you sure you want to continue? (Y/N) " confirm
   if [[ $confirm != "y" && $confirm != "Y" ]]; then
-    echo "Build cancelled. Exiting..."
+    echo "Build cancelled. Exiting"
     exit 1
   fi
 fi
 
-echo "🛠 Generating dist build..."
+echo "🛠 Generating dist build"
 yarn run webpack --config config/production.config.js
 
 echo "📃 Adding placeholder for statistics page"
 cp dist/index.html dist/stats.html
 
-echo "🏭 Generating build script..."
+echo "🏭 Generating build script"
 yarn run webpack --config config/cli-scripts.config.js
 
-echo "📄 Generating RSS feed and pages for each entry..."
+echo "📄 Generating RSS feed and pages for each entry"
 node script/generated/generateArtifacts.js
