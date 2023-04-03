12218caf96188cc32aaccafd1cf6b053f1a254dc
PakkuDon <pakku.pd@gmail.com>
2023-04-03 23:23:55 +1000

    Commit Prettier fixes

--- src/util/matchesSearchQuery.ts
+++ src/util/matchesSearchQuery.ts
@@ -3,9 +3,9 @@ import { PixelArtEntry } from "../data/index"
 const matchesSearchQuery = (searchQuery: string) => (entry: PixelArtEntry) => {
   searchQuery = searchQuery.toLowerCase()
   return (
-    entry.title && entry.title.toLowerCase().includes(searchQuery)) ||
+    (entry.title && entry.title.toLowerCase().includes(searchQuery)) ||
     entry.description?.toLowerCase().includes(searchQuery) ||
-    entry.tags.some((tag) => tag.toLowerCase() === searchQuery
+    entry.tags.some((tag) => tag.toLowerCase() === searchQuery)
   )
 }
 
