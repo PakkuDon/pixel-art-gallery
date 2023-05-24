5ccff80f2be0871d7ded0596840fdeaf43ddbb1f
PakkuDon <pakku.pd@gmail.com>
2023-05-24 23:30:20 +1000

    Update search predicate to search tags only when query starts with #

--- src/util/matchesSearchQuery.test.ts
+++ src/util/matchesSearchQuery.test.ts
@@ -2,56 +2,61 @@ import { matchesSearchQuery } from "./matchesSearchQuery"
 import { PixelArtEntry } from "../data"
 
 describe("matchesSearchQuery", () => {
-  it("matches when entry's title includes query regardless of case", () => {
-    const entry: PixelArtEntry = {
-      src: "",
-      title: "foobar",
-      palette: {
-        name: "",
-      },
-      resolution: "1x1",
-      date: "",
-      tags: [],
-    }
+  describe("when passed a plain string", () => {
+    it("matches when entry's title includes query regardless of case", () => {
+      const entry: PixelArtEntry = {
+        src: "",
+        title: "foobar",
+        palette: {
+          name: "",
+        },
+        resolution: "1x1",
+        date: "",
+        tags: [],
+      }
 
-    expect(matchesSearchQuery("foo", entry)).toBeTruthy()
-    expect(matchesSearchQuery("FOO", entry)).toBeTruthy()
-    expect(matchesSearchQuery("baR", entry)).toBeTruthy()
-    expect(matchesSearchQuery("something", entry)).toBeFalsy()
+      expect(matchesSearchQuery("foo", entry)).toBeTruthy()
+      expect(matchesSearchQuery("FOO", entry)).toBeTruthy()
+      expect(matchesSearchQuery("baR", entry)).toBeTruthy()
+      expect(matchesSearchQuery("something", entry)).toBeFalsy()
+    })
+
+    it("matches when entry's description includes query regardless of case", () => {
+      const entry: PixelArtEntry = {
+        src: "",
+        title: "",
+        description: "something",
+        palette: {
+          name: "",
+        },
+        resolution: "1x1",
+        date: "",
+        tags: [],
+      }
+
+      expect(matchesSearchQuery("something", entry)).toBeTruthy()
+      expect(matchesSearchQuery("SOME", entry)).toBeTruthy()
+    })
   })
 
-  it("matches when entry's description includes query regardless of case", () => {
-    const entry: PixelArtEntry = {
-      src: "",
-      title: "",
-      description: "something",
-      palette: {
-        name: "",
-      },
-      resolution: "1x1",
-      date: "",
-      tags: [],
-    }
+  describe("when passed a string starting with a #", () => {
+    it("matches when entry's tags include query regardless of case", () => {
+      const entry: PixelArtEntry = {
+        src: "",
+        title: "",
+        palette: {
+          name: "",
+        },
+        resolution: "1x1",
+        date: "",
+        tags: ["pixels", "landscape"],
+      }
 
-    expect(matchesSearchQuery("something", entry)).toBeTruthy()
-    expect(matchesSearchQuery("SOME", entry)).toBeTruthy()
-  })
-
-  it("matches when entry's tags include query regardless of case", () => {
-    const entry: PixelArtEntry = {
-      src: "",
-      title: "",
-      palette: {
-        name: "",
-      },
-      resolution: "1x1",
-      date: "",
-      tags: ["pixels", "landscape"],
-    }
-
-    expect(matchesSearchQuery("pixels", entry)).toBeTruthy()
-    expect(matchesSearchQuery("pixelS", entry)).toBeTruthy()
-    expect(matchesSearchQuery("landscape", entry)).toBeTruthy()
-    expect(matchesSearchQuery("land", entry)).toBeFalsy()
+      expect(matchesSearchQuery("#pixels", entry)).toBeTruthy()
+      expect(matchesSearchQuery("#pixelS", entry)).toBeTruthy()
+      expect(matchesSearchQuery("#landscape", entry)).toBeTruthy()
+      expect(matchesSearchQuery("#land", entry)).toBeFalsy()
+      expect(matchesSearchQuery("landscape", entry)).toBeFalsy()
+    })
   })
 })
--- src/util/matchesSearchQuery.ts
+++ src/util/matchesSearchQuery.ts
@@ -2,10 +2,13 @@ import { PixelArtEntry } from "../data/index"
 
 const matchesSearchQuery = (searchQuery: string, entry: PixelArtEntry) => {
   const lowerCaseSearchQuery = searchQuery.toLowerCase()
+  if (lowerCaseSearchQuery.startsWith("#")) {
+    const searchTag = lowerCaseSearchQuery.substring(1)
+    return entry.tags.some((tag) => tag.toLowerCase() === searchTag)
+  }
   return (
     (entry.title && entry.title.toLowerCase().includes(lowerCaseSearchQuery)) ||
-    entry.description?.toLowerCase().includes(lowerCaseSearchQuery) ||
-    entry.tags.some((tag) => tag.toLowerCase() === lowerCaseSearchQuery)
+    entry.description?.toLowerCase().includes(lowerCaseSearchQuery) || false
   )
 }
 
