a3df1b8afc972e20e14a3b0be264a2579d1973c1
PakkuDon <pakku.pd@gmail.com>
2023-06-23 20:00:26 +1000

    Refactor <TagList /> to use <Accordion /> component

--- src/components/Sidebar/TagList/TagList.tsx
+++ src/components/Sidebar/TagList/TagList.tsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 import { Link } from "react-router-dom"
+
+import { Accordion } from "../../Accordion/Accordion"
 import { encodeURIFragment } from "../../../util/encodeURIFragment"
 
 interface TagListProps {
@@ -10,37 +12,18 @@ interface TagListProps {
 const TagList = ({
   countByTag,
   limit = Number.MAX_SAFE_INTEGER,
-}: TagListProps) => {
-  const [visible, setVisible] = useState(false)
-
-  if (visible) {
-    return (
-      <div>
-        <div>
-          <button type="button" onClick={() => setVisible(false)}>
-            Hide most used tags
-          </button>
-        </div>
-        {countByTag.slice(0, limit).map(({ tag, count }, index) => (
-          <React.Fragment key={`tag-with-count-${tag}`}>
-            {/* "%2523" is '#' encoded */}
-            <Link to={encodeURIFragment(`?q=#${tag}`)}>
-              {tag} ({count})
-            </Link>
-            {index < countByTag.length - 1 && ", "}
-          </React.Fragment>
-        ))}
-      </div>
-    )
-  }
-
-  return (
-    <div>
-      <button type="button" onClick={() => setVisible(true)}>
-        Show most used tags
-      </button>
-    </div>
-  )
-}
+}: TagListProps) => (
+  <Accordion openLabel="Show most used tags" closeLabel="Hide most used tags">
+    {countByTag.slice(0, limit).map(({ tag, count }, index) => (
+      <React.Fragment key={`tag-with-count-${tag}`}>
+        {/* "%2523" is '#' encoded */}
+        <Link to={encodeURIFragment(`?q=#${tag}`)}>
+          {tag} ({count})
+        </Link>
+        {index < countByTag.length - 1 && ", "}
+      </React.Fragment>
+    ))}
+  </Accordion>
+)
 
 export { TagList }
