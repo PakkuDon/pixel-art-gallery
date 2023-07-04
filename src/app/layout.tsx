cbea0f9d82fa3d684709db86f55a80aa9be78dd4
PakkuDon <pakku.pd@gmail.com>
2023-07-04 22:39:40 +1000

    Create root layout

---
+++ src/app/layout.tsx
@@ -0,0 +1,57 @@
+'use client'
+import React, { useEffect, useState } from "react"
+import "normalize.css"
+
+import { PixelArtRepository } from "../PixelArtRepository"
+import "./styles.css"
+
+PixelArtRepository.load()
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+const RootLayout = ({ children }: LayoutProps) => {
+  const [prefersDarkTheme, setPrefersDarkTheme] = useState(false)
+
+  useEffect(() => {
+    if (
+      window.matchMedia &&
+      window.matchMedia("(prefers-color-scheme: dark)").matches
+    ) {
+      setPrefersDarkTheme(true)
+    }
+  }, [])
+
+  useEffect(() => {
+    document.body.classList.remove("dark-theme", "light-theme")
+    document.body.classList.add(prefersDarkTheme ? "dark-theme" : "light-theme")
+  }, [prefersDarkTheme])
+
+  return (
+    <html lang="en">
+      <head>
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Pixel art by PakkuDon." />
+        <meta property="og:description" content="Pixel art by PakkuDon." />
+        <meta name="twitter:card" content="summary" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin=""
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans&display=swap"
+          rel="stylesheet"
+        />
+        <title>Pixel Art Gallery</title>
+      </head>
+      <body>
+        {children}
+      </body>
+    </html>
+  )
+}
+
+export default RootLayout
