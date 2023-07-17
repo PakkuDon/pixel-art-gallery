22f2c3f5286b6a972d44e97c246fddbbd4a386f6
PakkuDon <pakku.pd@gmail.com>
2023-07-17 13:24:28 +1000

    Update tests for <ImageDetails />
    - Props have changed
    - Mock useSearchParams as this is used to determine which
    images to link to

--- src/app/[[...id]]/ImageDetails.test.tsx
+++ src/app/[[...id]]/ImageDetails.test.tsx
@@ -1,7 +1,16 @@
 import React from "react"
 import { screen, render } from "@testing-library/react"
 
-import ImageDetails from "./page"
+import { ImageDetails } from "./ImageDetails"
+import { PixelArtRepository } from "../../PixelArtRepository"
+
+// Required as <ImageDetails /> renders a pagination component
+// which uses Next.js' useSearchParms function to determine
+// which images to link to
+jest.mock("next/navigation", () => ({
+  ...jest.requireActual("next/navigation"),
+  useSearchParams: () => ({ get: () => "" }),
+}))
 
 describe("ImageDetails", () => {
   it("renders image details", () => {
@@ -16,6 +25,7 @@ describe("ImageDetails", () => {
       date: "2000-01-01T00:00+00:00",
       tags: ["test"],
     }
+    PixelArtRepository.load([img])
 
     render(<ImageDetails image={img} />)
 
@@ -26,7 +36,7 @@ describe("ImageDetails", () => {
     expect(screen.getByText("test")).toBeInTheDocument()
   })
 
-  it("renders placeholders for navigational links if previous and next image not provided", () => {
+  it("renders placeholders for navigational links if previous and next image not available", () => {
     const img = {
       src: "abc123.png",
       title: "Test image",
@@ -38,6 +48,7 @@ describe("ImageDetails", () => {
       date: "2000-01-01T00:00+10:00",
       tags: ["test"],
     }
+    PixelArtRepository.load([img])
 
     render(<ImageDetails image={img} />)
 
@@ -66,8 +77,10 @@ describe("ImageDetails", () => {
       ...img,
       src: "nextImage.png",
     }
+    // Images loaded in reverse order as they are reversed in UI
+    PixelArtRepository.load([nextImage, img])
 
-    render(<ImageDetails image={img} nextImage={nextImage} />)
+    render(<ImageDetails image={img} />)
 
     expect(screen.getByRole("link", { name: "Next" })).toHaveAttribute(
       "href",
@@ -91,8 +104,10 @@ describe("ImageDetails", () => {
       ...img,
       src: "previousImage.png",
     }
+    // Images loaded in reverse order as they are reversed in UI
+    PixelArtRepository.load([img, previousImage])
 
-    render(<ImageDetails image={img} previousImage={previousImage} />)
+    render(<ImageDetails image={img} />)
 
     expect(screen.getByRole("link", { name: "Previous" })).toHaveAttribute(
       "href",
