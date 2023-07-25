040f75160800e892723b7cf54ca750b8c2fa782d
PakkuDon <pakku.pd@gmail.com>
2023-07-26 01:02:34 +1000

    Document env vars used in deployment process

--- next.config.js
+++ next.config.js
@@ -3,7 +3,14 @@
  */
 
 const path = require("path")
+
+// BASE_PATH defines what subpath the application will be hosted at
+// This can be used to see how the app will behave on Github Pages
 const basePath = process.env.BASE_PATH ?? ""
+
+// IS_DEPLOY determines whether or not the dist bundle should include
+// the basePath. This is not required on Github Pages as the files are
+// hosted at the basePath (assuming basePath matches repo name)
 const distDir = process.env.IS_DEPLOY ? "dist" : path.join("dist", basePath)
 
 const nextConfig = {
