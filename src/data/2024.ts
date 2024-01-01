45e01490a6f51aa214166c3ea9e05c46fc209dea
PakkuDon <pakku.pd@gmail.com>
2024-01-01 23:58:25 +1100

    Add 20240101-pixeldailies

--- src/data/2024.ts
+++ src/data/2024.ts
@@ -1,5 +1,18 @@
 import { PixelArtEntry } from "."
 
-const entries: PixelArtEntry[] = []
+const entries: PixelArtEntry[] = [
+  {
+    src: "2024/20240101-pixeldailies.png",
+    title: "Pixel Dailies prompt: #Dough",
+    description: ``,
+    palette: {
+      name: "ZyKro-48",
+      url: "https://lospec.com/palette-list/zykro-48",
+    },
+    resolution: "64x64",
+    date: "2024-01-01T23:58+11:00",
+    tags: ["pixel_dailies", "Dough", "food", "64x64", "zykro-48"],
+  },
+]
 
 export { entries }
