f618bac4e9ffb0f2930bcad973cc0fca1ec923fb
PakkuDon <pakku.pd@gmail.com>
2024-02-19 23:25:21 +1100

    Only set distDir in production
    There's currently a bug in Next.js 14.1 where
    the dev server will not run if both `distDir` and
    `output: "export"` are set in Next.js' config
    Reference: https://github.com/vercel/next.js/issues/61105

--- next.config.js
+++ next.config.js
@@ -11,7 +11,16 @@ const basePath = process.env.NEXT_PUBLIC_BASE_PATH ?? ""
 // IS_DEPLOY determines whether or not the dist bundle should include
 // the basePath. This is not required on Github Pages as the files are
 // hosted at the basePath (assuming basePath matches repo name)
-const distDir = process.env.IS_DEPLOY ? "dist" : path.join("dist", basePath)
+let distDir
+
+// Clear distDir to resolve an issue where the dev server
+// won't compile if both distDir and output: "export" are set
+// See https://github.com/vercel/next.js/issues/61105
+if (process.env.NODE_ENV === "development") {
+  distDir = undefined
+} else {
+  distDir = process.env.IS_DEPLOY ? "dist" : path.join("dist", basePath)
+}
 
 const nextConfig = {
   output: "export",
