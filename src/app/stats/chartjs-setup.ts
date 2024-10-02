a6a86c2e56cefaf1fa62d4699245908ed889dc8d
PakkuDon <pakku.pd@gmail.com>
2024-10-02 22:35:58 +1000

    Disable typecheck for chartjs-plugin-autocolors
    Without this @ts-ignore the build fails with the
    following error message:
    
    > Type error: Module '"~/node_modules/chartjs-plugin-autocolors/types/index.esm"'
    has no default export.

--- src/app/stats/chartjs-setup.ts
+++ src/app/stats/chartjs-setup.ts
@@ -5,6 +5,8 @@ import {
   BarElement,
   Tooltip,
 } from "chart.js"
+// Disable type-check otherwise build fails with Type error: Module '"..."' has no default export.
+// @ts-ignore
 import autocolors from "chartjs-plugin-autocolors"
 
 ChartJS.register(autocolors, CategoryScale, LinearScale, BarElement, Tooltip)
