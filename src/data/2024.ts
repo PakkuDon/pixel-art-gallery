fdf3a945af608f586ff7ff2cd24326e3a178782c
PakkuDon <pakku.pd@gmail.com>
2024-12-09 23:31:37 +1100

    Add 20241209-pixel-artvent

--- src/data/2024.ts
+++ src/data/2024.ts
@@ -575,6 +575,18 @@ const entries: PixelArtEntry[] = [
     date: "2024-12-08T23:44+11:00",
     tags: ["pixel-artvent", "SantaHat", "christmas", "16x16", "apollo"],
   },
+  {
+    src: "2024/20241209-pixel-artvent.png",
+    title: "Pixel Artvent prompt: #Candle",
+    description: `[Prompt list for Pixel Artvent](https://bsky.app/profile/pixelartjourney.bsky.social/post/3lc6cgoxds22n)`,
+    palette: {
+      name: "The Crow",
+      url: "https://lospec.com/palette-list/the-crow",
+    },
+    resolution: "16x16",
+    date: "2024-12-09T23:31+11:00",
+    tags: ["pixel-artvent", "Candle", "christmas", "16x16", "the-crow"],
+  },
 ]
 
 export { entries }
