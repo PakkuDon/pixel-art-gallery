2f482ea9c644b2d44e081ebce23e2e91febce4a5
PakkuDon <pakku.pd@gmail.com>
2025-01-01 17:52:45 +1100

    Add data file for 2025 entries

--- src/data/index.ts
+++ src/data/index.ts
@@ -3,6 +3,7 @@ import { entries as entries2021 } from "./2021"
 import { entries as entries2022 } from "./2022"
 import { entries as entries2023 } from "./2023"
 import { entries as entries2024 } from "./2024"
+import { entries as entries2025 } from "./2025"
 
 export interface PixelArtEntry {
   src: string
@@ -25,6 +26,7 @@ const entries: PixelArtEntry[] = [
   ...entries2022,
   ...entries2023,
   ...entries2024,
+  ...entries2025,
 ]
 
 export { entries }
