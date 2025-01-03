ace6bc2e40cc9ed17d54c1995236f6a1a2821d16
PakkuDon <pakku.pd@gmail.com>
2025-01-03 17:52:01 +1100

    Add 20250103-pixeldailies

--- src/data/2025.ts
+++ src/data/2025.ts
@@ -1,5 +1,18 @@
 import { PixelArtEntry } from "."
 
-const entries: PixelArtEntry[] = []
+const entries: PixelArtEntry[] = [
+  {
+    src: "2025/20250103-pixeldailies.png",
+    title: "Pixel Dailies prompt: #MountainTop",
+    description: ``,
+    palette: {
+      name: "The Crow",
+      url: "https://lospec.com/palette-list/the-crow",
+    },
+    resolution: "64x64",
+    date: "2025-01-03T17:52+11:00",
+    tags: ["pixel_dailies", "MountainTop", "landscape", "64x64", "the-crow"],
+  },
+]
 
 export { entries }
