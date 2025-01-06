8fc5115012533655176a97d327c976e73680b136
PakkuDon <pakku.pd@gmail.com>
2025-01-06 23:59:36 +1100

    Add 20250106-pixeldailies

--- src/data/2025.ts
+++ src/data/2025.ts
@@ -13,6 +13,18 @@ const entries: PixelArtEntry[] = [
     date: "2025-01-03T17:52+11:00",
     tags: ["pixel_dailies", "MountainTop", "landscape", "64x64", "the-crow"],
   },
+  {
+    src: "2025/20250106-pixeldailies.png",
+    title: "Pixel Dailies prompt: #strawberry",
+    description: ``,
+    palette: {
+      name: "Moor",
+      url: "https://lospec.com/palette-list/moor",
+    },
+    resolution: "64x64",
+    date: "2025-01-06T23:59+11:00",
+    tags: ["pixel_dailies", "strawberry", "fruit", "food", "64x64", "moor"],
+  },
 ]
 
 export { entries }
