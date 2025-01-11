2590d2050cd61c25a396917cefbbd7e7a00f9342
PakkuDon <pakku.pd@gmail.com>
2025-01-11 22:25:09 +1100

    Add 20250111-pixeldailies

--- src/data/2025.ts
+++ src/data/2025.ts
@@ -25,6 +25,18 @@ const entries: PixelArtEntry[] = [
     date: "2025-01-06T23:59+11:00",
     tags: ["pixel_dailies", "strawberry", "fruit", "food", "64x64", "moor"],
   },
+  {
+    src: "2025/20250111-pixeldailies.png",
+    title: "Pixel Dailies prompt: #pearl",
+    description: `This post done with the support of [Rappenem's sphere tutorial](https://www.patreon.com/posts/pixel-art-sphere-108853066)`,
+    palette: {
+      name: "The Crow",
+      url: "https://lospec.com/palette-list/the-crow",
+    },
+    resolution: "64x64",
+    date: "2025-01-11T22:25+11:00",
+    tags: ["pixel_dailies", "pearl", "64x64", "the-crow"],
+  },
 ]
 
 export { entries }
